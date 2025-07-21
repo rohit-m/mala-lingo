cb8d7e0a58241621b7f52cc63664da8e5c1352f4
Rohit Mohan <rohit.mohan2@gmail.com>
2025-07-21 22:21:51 +0100

    Remove pages that we dont need

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,24 +9,6 @@ const routes = [
     meta: { title: 'Mala Lingo - Home' }
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue'),
-    meta: { title: 'Mala Lingo - Login' }
-  },
-  {
-    path: '/signup',
-    name: 'Signup',
-    component: () => import('../views/Signup.vue'),
-    meta: { title: 'Mala Lingo - Sign Up' }
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: () => import('../views/Dashboard.vue'),
-    meta: { requiresAuth: true, title: 'Mala Lingo - Dashboard' }
-  },
-  {
     path: '/matching-words',
     name: 'MatchingWords',
     component: () => import('../views/MatchingWords.vue'),
@@ -38,24 +20,6 @@ const routes = [
     component: () => import('../views/FlipTest.vue'),
     meta: { title: 'Mala Lingo - Flip Test' }
   },
-  {
-    path: '/lessons',
-    name: 'Lessons',
-    component: () => import('../views/Lessons.vue'),
-    meta: { title: 'Mala Lingo - Lessons' }
-  },
-  {
-    path: '/lesson/:id',
-    name: 'LessonDetail',
-    component: () => import('../views/LessonDetail.vue'),
-    meta: { title: 'Mala Lingo - Lesson Details' }
-  },
-  {
-    path: '/practice/:id',
-    name: 'WordPool',
-    component: () => import('../views/WordPool.vue'),
-    meta: { title: 'Mala Lingo - Practice' }
-  }
 ]
 
 const router = createRouter({
